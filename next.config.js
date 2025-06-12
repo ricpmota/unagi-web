b09d9cd6a122e49da4c534a10d13b57be06fa4bc
ricpmota <158510032+ricpmota@users.noreply.github.com>
2025-06-12 12:34:25 -0300

    fix: ajusta configurações do Next.js e versão do Firebase

--- next.config.js
+++ next.config.js
@@ -1,1 +1,20 @@
-module.exports = { reactStrictMode: true }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+        dns: false,
+        child_process: false,
+        perf_hooks: false,
+      };
+    }
+    return config;
+  },
+}
+
+module.exports = nextConfig
